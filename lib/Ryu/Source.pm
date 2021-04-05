e049e4c930728846b819c22ba97be3b6d4fb4536
Tom Molesworth <TEAM@cpan.org>
2021-04-05 12:12:55 +0800

    Track pending futures

--- lib/Ryu/Source.pm
+++ lib/Ryu/Source.pm
@@ -1191,10 +1191,10 @@ sub ordered_futures {
         $_->cancel for grep { $_ and not $_->is_ready } @pending;
     });
     $self->each(sub {
-        my $k = Scalar::Util::refaddr $_;
-        $pending{$k} = 1;
-        $log->tracef('Ordered futures has %d pending', 0 + keys %pending);
         my $f = $_;
+        my $k = Scalar::Util::refaddr $f;
+        $pending{$k} = $f;
+        $log->tracef('Ordered futures has %d pending', 0 + keys %pending);
         $_->on_done(sub {
             my @pending = @_;
             while(@pending and not $src_completed->is_ready) {
