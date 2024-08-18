e810e568ce105a37dbbcc87a694e2ab8c438a635
Tom Molesworth <TEAM@cpan.org>
2024-08-19 01:08:23 +0800

    No need for the ->without_cancel branch when describing state (readonly)

--- lib/Ryu/Node.pm
+++ lib/Ryu/Node.pm
@@ -43,7 +43,7 @@ like C<< from->combine_latest->count >>.
 # It'd be nice if L<Future> already provided a method for this, maybe I should suggest it
 sub describe {
     my ($self) = @_;
-    my $completed = $self->completed;
+    my $completed = $self->_completed;
     ($self->parent ? $self->parent->describe . '=>' : '') . '[' . ($self->label // 'unknown') . '](' . ($completed ? $completed->state : 'inactive') . ')';
 }
 
